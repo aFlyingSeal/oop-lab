21c08c73bfdee9ed9766f7ce5e83ca28e270b754
Dat Le <thanhdat952006@gmail.com>
2025-11-11 15:53:07 +0700

    finished Bai4.cpp

--- W04/Bai4.cpp
+++ W04/Bai4.cpp
@@ -1,6 +1,84 @@
 #include "LinkedList.h"
-#include<ctime>
-#include<cstdlib>
+
+class CString{
+private:
+    char* arr;
+public:
+    CString();
+    CString(const char* str);
+    CString(const CString& other);
+
+    CString& operator++();
+    CString operator++(int);
+
+    char& operator[](int idx);
+    CString& operator=(const CString& other);
+    friend ostream& operator<<(ostream& out, const CString& other);
+
+    ~CString();
+};
+
+CString::CString(){
+    arr = nullptr;
+}
+
+CString::CString(const char* str){
+    int size = strlen(str);
+    arr = new char[size + 1];
+    strcpy(arr, str);
+}
+
+CString::CString(const CString& other): CString(other.arr){
+}
+
+CString& CString::operator++(){
+    if (arr != nullptr){
+        arr[strlen(arr) - 1]++;
+    }
+    return *this;
+}
+
+CString CString::operator++(int){
+    CString tmp = *this;
+    if (arr != nullptr){
+        arr[strlen(arr) - 1]++;
+    }
+    return tmp;
+}
+
+char& CString::operator[](int idx){
+    if (idx < 0 || idx >= strlen(arr)){
+        throw out_of_range("out of range");
+    }
+    return arr[idx];
+}
+
+CString& CString::operator=(const CString& other){
+    if (this != &other){
+        delete[] arr;
+        if (other.arr != nullptr){
+            int size = strlen(other.arr);
+            arr = new char[size + 1];
+            strcpy(arr, other.arr);
+        }
+        else{
+            arr = nullptr;
+        }
+    }
+    return *this;
+}
+
+ostream& operator<<(ostream& out, const CString& s){
+    out << s.arr;
+    return out;
+}
+
+CString::~CString(){
+    if (arr != nullptr){
+        delete[] arr;
+        arr = nullptr;
+    }
+}
 
 int main(){
     srand((unsigned int)time(NULL));
@@ -23,13 +101,13 @@ int main(){
     cout << "Sau thay doi: " << endl;
     cout << ll << endl;
 
-    // LinkedList<CString> llCS;
-    // llCS.AddTail(CString("cstring-1"));
-    // llCS.AddTail(CString("cstring-2"));
-    // llCS.AddTail(CString("cstring-3"));
-    // cout << "List CString: " << llCS << endl;
-    // llCS[iTest] = CString("new CString");
-    // cout << "List CString changed: " << endl << llCS << endl;
-    // system("pause");
-    // return 0;
+    LinkedList<CString> llCS;
+    llCS.AddTail(CString("cstring-1"));
+    llCS.AddTail(CString("cstring-2"));
+    llCS.AddTail(CString("cstring-3"));
+    cout << "List CString: " << llCS << endl;
+    llCS[iTest] = CString("new CString");
+    cout << "List CString changed: " << endl << llCS << endl;
+    system("pause");
+    return 0;
 }
